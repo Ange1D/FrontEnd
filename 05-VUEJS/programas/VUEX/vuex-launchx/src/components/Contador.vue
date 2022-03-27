0fe263cb9ae77fde15c2b14767b45e9be549ef13
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-03-27 12:27:07 -0600

    vue code

---
+++ 05-VUEJS/programas/VUEX/vuex-launchx/src/components/Contador.vue
@@ -0,0 +1,12 @@
+<template>
+    <div :style={color:$store.state.color} class="contador">
+      {{ $store.state.contador }}
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Contador'
+}
+</script>
